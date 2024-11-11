73b2b76ff30ac64495dbd33c3f168102b0ba3037
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-11-11 20:30:43 +0530

    Authentication via clerk

---
+++ convex/users.ts
@@ -0,0 +1,61 @@
+import { ConvexError, v } from "convex/values";
+import { internalMutation } from "./_generated/server";
+
+export const createUser = internalMutation({
+  args: {
+    tokenIdentifier: v.string(),
+    email: v.string(),
+    name: v.string(),
+    image: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("users", {
+      tokenIdentifier: args.tokenIdentifier,
+      email: args.email,
+      name: args.name,
+      image: args.image,
+    });
+  },
+});
+
+export const updateUser = internalMutation({
+  args: { tokenIdentifier: v.string(), image: v.string() },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_tokenIdentifier", (q) =>
+        q.eq("tokenIdentifier", args.tokenIdentifier)
+      )
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.patch(user._id, {
+      image: args.image,
+    });
+
+    console.log("User updated");
+  },
+});
+
+export const deleteUser = internalMutation({
+  args: { tokenIdentifier: v.string() },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_tokenIdentifier", (q) =>
+        q.eq("tokenIdentifier", args.tokenIdentifier)
+      )
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.delete(user._id);
+
+    console.log("User deleted");
+  },
+});
